62929702e1dfd903d4e4c171b7b3ee9ae9f28005
Kwothsei <45246543+Kwothsei@users.noreply.github.com>
2020-05-23 18:50:09 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Diagnostics;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestWeb.Models;
+
+namespace TestWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+            => View();
+
+        public IActionResult Privacy()
+            => View();
+
+        public IActionResult CV()
+            => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
